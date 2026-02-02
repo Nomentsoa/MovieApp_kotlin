abb59b645191b108d61be32316e446dfbf80423a
Laza_Nomentsoa_Sedera <starksedera@gmail.com>
2026-02-02 10:51:08 -0500

    finalisation application

--- app/src/main/java/com/lazanomentsoarabesandratana/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/lazanomentsoarabesandratana/movieapp/screens/details/DetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.lazanomentsoarabesandratana.movieapp.screens.details
 
-import android.provider.CalendarContract
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,9 +7,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,16 +29,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.lazanomentsoarabesandratana.movieapp.R
+import com.lazanomentsoarabesandratana.movieapp.model.Movie
+import com.lazanomentsoarabesandratana.movieapp.model.getMovie
+import com.lazanomentsoarabesandratana.movieapp.widgets.MovieRow
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun DetailsScreen(navController: NavController= rememberNavController(), movieData: String?= null){
+fun DetailsScreen(navController: NavController= rememberNavController(), movieId: String?= null){
+    val movie = getMovie().filter { movie -> movie.id == movieId
+    }[0]
     Scaffold(
         topBar = {
             TopAppBar(
@@ -66,18 +80,23 @@ fun DetailsScreen(navController: NavController= rememberNavController(), movieDa
         }
     ) {
         paddingValues ->
-        MainDetaisScreen(navController = navController, modifier = Modifier.padding(paddingValues), movieData = movieData)
+        MainDetaisScreen(navController = navController, modifier = Modifier.padding(paddingValues), movie = movie)
     }
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainDetaisScreen(navController: NavController = rememberNavController(), modifier: Modifier = Modifier, movieData: String? = null){
+fun MainDetaisScreen(navController: NavController = rememberNavController(), modifier: Modifier = Modifier, movie: Movie? = null){
     Column(modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Movie data $movieData",
+        MovieRow (movie = movie!!)
+        Spacer(modifier = Modifier.height(8.dp))
+        Divider()
+        horizontalScrollableImageView(movie)
+
+        /*Text("Movie data ${movie?.title}",
             style = MaterialTheme.typography.headlineLarge)
         Spacer(modifier = Modifier.height(23.dp))
         Button(onClick = {
@@ -85,6 +104,33 @@ fun MainDetaisScreen(navController: NavController = rememberNavController(), mod
             navController.popBackStack()
         }) {
             Text(text = "Go Back")
+        }*/
+    }
+}
+
+@Composable
+private fun horizontalScrollableImageView(movie: Movie) {
+    LazyRow {
+        items(movie.images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = CardDefaults.cardElevation(5.dp)
+            ) {
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = image,
+                    contentDescription = "Movie poster",
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(R.drawable.ic_launcher_background),
+                    error = painterResource(R.drawable.ic_launcher_foreground),
+                    onError = { error ->
+                        // Log the error cause to Logcat
+                        Log.d("Error", " error: ${error.result.throwable.message}")
+                    }
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/lazanomentsoarabesandratana/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/lazanomentsoarabesandratana/movieapp/widgets/Widgets.kt
@@ -2,7 +2,9 @@ package com.lazanomentsoarabesandratana.movieapp.widgets
 
 import android.R.attr.contentDescription
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,14 +12,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,8 +37,13 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
@@ -38,11 +55,14 @@ import com.lazanomentsoarabesandratana.movieapp.model.getMovie
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovie()[0], onItemClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp),
+            .wrapContentHeight(),
         onClick = {
             onItemClick(movie.id)
         },
@@ -58,16 +78,7 @@ fun MovieRow(movie: Movie = getMovie()[0], onItemClick: (String) -> Unit = {}) {
         ) {
 
 
-
-            /*    Image(modifier = Modifier.size(50.dp),
-                    painter = rememberImagePainter(movie.images[0]), // Replace with your image URL
-                    contentDescription = "Description of the image",
-                    // Optional: add a placeholder or error image
-                    // placeholder = painterResource(R.drawable.your_placeholder),
-                    // error = painterResource(R.drawable.your_error_image)
-                )
-*/
-            AsyncImage( modifier = Modifier.size(120.dp, 130.dp),
+            AsyncImage( modifier = Modifier.size(120.dp, 130.dp).padding(10.dp),
                 model = movie.images[0],
                 contentDescription = "Movie poster",
                 contentScale = ContentScale.Crop,
@@ -80,13 +91,42 @@ fun MovieRow(movie: Movie = getMovie()[0], onItemClick: (String) -> Unit = {}) {
             )
 
             Column(modifier = Modifier.padding(4.dp)) {
-                Text(text = movie.title, style = MaterialTheme.typography.headlineLarge)
+                Text(text = movie.title, style = MaterialTheme.typography.headlineMedium)
                 Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.titleMedium)
                 Text(text = "Released: ${movie.year}")
+                AnimatedVisibility(visible = expanded) {
+                    Column(modifier = Modifier.padding(6.dp)) {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)){
+                                append("Plot: ")
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp, fontWeight = FontWeight.Light)){
+                                append(movie.plot)
+                            }
+                        })
+                        Divider(modifier = Modifier.padding(3.dp))
+                        Text(text = "Director: ${movie.director}")
+                        Text(text = "Actors: ${movie.actors}")
+                        Text(text = "Rating: ${movie.rating}")
+
+
+                    }
+                }
+
+                Icon(imageVector = if(expanded )Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier.size(25.dp)
+                        .clickable{
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray)
             }
 
 
+
         }
+
+
 
     }
 }
